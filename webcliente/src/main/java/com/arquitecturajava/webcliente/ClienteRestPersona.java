5c5aec09ceac5182058ff42ef4993734de91ac9f
amoretabilbo <amoreta@bilbomatica.es>
2024-11-15 14:01:48 +0100

    Clietne inicio

---
+++ webcliente/src/main/java/com/arquitecturajava/webcliente/ClienteRestPersona.java
@@ -0,0 +1,27 @@
+package com.arquitecturajava.webcliente;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.arquitecturajava.webspring.dtos.PersonaDTO;
+
+@RestController
+public class ClienteRestPersona {
+	
+	@RequestMapping("/personascliente")
+	public List<PersonaDTO> listaPersonas() {
+		
+		RestTemplate plantilla = new RestTemplate();
+		
+		ResponseEntity<PersonaDTO[]> lista = plantilla.getForEntity("http://localhost:8083/personas", PersonaDTO[].class);
+		PersonaDTO[] nuevaLista = lista.getBody();
+		return Arrays.asList(nuevaLista);
+	}
+
+}
